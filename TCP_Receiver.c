c9c15f9b81726e2c0e497a601fa16371e31088eb
OmerApt <156093828+OmerApt@users.noreply.github.com>
2024-02-27 22:35:59 +0200

    we start something

---
+++ TCP_Receiver.c
@@ -0,0 +1,237 @@
+// includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <time.h>
+//
+
+// defines
+#define DEFAULT_SERVER_PORT 55447
+#define NUM_OF_CLINETS 1
+#define DEFAULT_ALGO "reno"
+#define BUFFER_SIZE 1024
+//
+
+int main(int argsc, char **argsv)
+{
+
+    // char *rnd_file_buffer = util_generate_random_data(2500000);
+    char *tcp_algo = DEFAULT_ALGO;
+    // char *ip_address = DEFAULT_SERVER_IP_ADDRESS;
+    int port_Address = DEFAULT_SERVER_PORT;
+
+    if (argsc <= 1)
+    {
+        // no arguments
+    }
+    else
+    {
+
+        int i = 1;
+        while (i < argsc)
+        {
+            char *arg = argsv[i];
+            // printf("args num is %d\n", argsc);
+            if (strcmp(arg, "-p") == 0)
+            {
+                i++;
+                if (i == argsc)
+                {
+                    // error empty arg
+                }
+                else
+                {
+                    port_Address = atoi(argsv[i]);
+                    // printf("%d\n", port_Address);
+                }
+            }
+            else if (strcmp(arg, "-algo") == 0)
+            {
+                i++;
+                if (i == argsc)
+                {
+                    // error empty arg
+                }
+                else
+                {
+                    // check if value is reno or cubic
+                    tcp_algo = argsv[i];
+                }
+            }
+            i++;
+        }
+    }
+
+    //// finished args reading
+
+    // The variable to store the socket file descriptor.
+    int sock = -1;
+
+    // The variable to store the server's address.
+    struct sockaddr_in server;
+
+    // The variable to store the client's address.
+    struct sockaddr_in client;
+
+    // Stores the client's structure length.
+    socklen_t client_len = sizeof(client);
+
+    // The variable to store the socket option for reusing the server's address.
+    int opt = 1;
+
+    // Reset the server and client structures to zeros.
+    memset(&server, 0, sizeof(server));
+    memset(&client, 0, sizeof(client));
+
+    // Try to create a TCP socket (IPv4, stream-based, default protocol).
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock == -1)
+    {
+        perror("socket(2)");
+        return 1;
+    }
+    // Set the TCP congestion algorithm option for this socket
+    char alg_buf[256];
+    socklen_t alg_buf_len;
+
+    strcpy(alg_buf, tcp_algo);
+    alg_buf_len = sizeof(alg_buf);
+
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, alg_buf, alg_buf_len) != 0)
+    {
+        // Option set failed
+        perror("setsockopt(congestion)");
+        close(sock);
+        return 1;
+    }
+
+    // Set the socket option to reuse the server's address.
+    // This is useful to avoid the "Address already in use" error message when restarting the server.
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        perror("setsockopt(2)");
+        close(sock);
+        return 1;
+    }
+
+    // Set the server's address to "0.0.0.0" (all IP addresses on the local machine).
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    // Set the server's address family to AF_INET (IPv4).
+    server.sin_family = AF_INET;
+
+    // Set the server's port to the specified port. Note that the port must be in network byte order.
+    server.sin_port = htons(port_Address);
+
+    // Try to bind the socket to the server's address and port.
+    if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        perror("bind(2)");
+        close(sock);
+        return 1;
+    }
+
+    // Try to listen for incoming connections.
+    if (listen(sock, NUM_OF_CLINETS) < 0)
+    {
+        perror("listen(2)");
+        close(sock);
+        return 1;
+    }
+
+    fprintf(stdout, "Listening for incoming connections on port %d...\n", port_Address);
+
+    // double total_of_all;
+
+    // The server's main loop.
+    while (1)
+    {
+        // Try to accept a new client connection.
+        int client_sock = accept(sock, (struct sockaddr *)&client, &client_len);
+        clock_t start_time,endtime;
+        double total;
+        // timer_t end_time = 0;
+        start_time = clock();
+        // If the accept call failed, print an error message and return 1.
+        if (client_sock < 0)
+        {
+            perror("accept(2)");
+            close(sock);
+            return 1;
+        }
+
+        // Print a message to the standard output to indicate that a new client has connected.
+        fprintf(stdout, "Client %s:%d connected\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+        // Create a buffer to store the received message.
+        char buffer[BUFFER_SIZE] = {0};
+
+        // Receive a message from the client and store it in the buffer.
+        int bytes_received = recv(client_sock, buffer, BUFFER_SIZE, 0);
+
+        // If the message receiving failed, print an error message and return 1.
+        if (bytes_received < 0)
+        {
+            perror("recv(2)");
+            close(client_sock);
+            close(sock);
+            return 1;
+        }
+        // If the amount of received bytes is 0, the client has disconnected.
+        // Close the client's socket and continue to the next iteration.
+        else if (bytes_received == 0)
+        {
+            fprintf(stdout, "Client %s:%d disconnected\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+            close(client_sock);
+            continue;
+        }
+        endtime = clock();
+        total = (double)(endtime-start_time)/CLOCKS_PER_SEC;
+        total *= 1000;
+
+        printf("total time = %f\n",total);
+
+        // Ensure that the buffer is null-terminated, no matter what message was received.
+        // This is important to avoid SEGFAULTs when printing the buffer.
+        if (buffer[BUFFER_SIZE - 1] != '\0')
+            buffer[BUFFER_SIZE - 1] = '\0';
+
+        fprintf(stdout, "Received %d bytes from the client %s:%d\n", bytes_received, inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+        // Send back a message to the client.
+        /*
+
+
+        int bytes_sent = send(client_sock, message, messageLen, 0);
+
+        // If the message sending failed, print an error message and return 1.
+        // We do not need to check for 0 bytes sent, as if the client disconnected, we would have already closed the socket.
+        if (bytes_sent < 0)
+        {
+            perror("send(2)");
+            close(client_sock);
+            close(sock);
+            return 1;
+        }
+
+        fprintf(stdout, "Sent %d bytes to the client %s:%d!\n", bytes_sent, inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+
+        */
+        // Close the client's socket and continue to the next iteration.
+        close(client_sock);
+
+        fprintf(stdout, "Client %s:%d disconnected\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+    }
+
+    fprintf(stdout, "Server finished!\n");
+
+    return 0;
+}
---
+++ TCP_Sender.c
@@ -0,0 +1,253 @@
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+//
+
+// define
+#define DEFAULT_SERVER_PORT 55447
+#define DEFAULT_SERVER_IP_ADDRESS "127.0.0.1"
+#define DEFAULT_ALGO "reno"
+#define NUM_OF_RUNS 10
+//
+
+int sendall(int s, char *buf, int *len);
+char *util_generate_random_data(unsigned int size);
+int user_cont();
+
+int main(int argsc, char **argsv)
+{
+    int data_size = pow(2, 25);
+    char *rnd_file_buffer = util_generate_random_data(data_size);
+    char *tcp_algo = DEFAULT_ALGO;
+    char *ip_address = DEFAULT_SERVER_IP_ADDRESS;
+    int port_Address = DEFAULT_SERVER_PORT;
+
+    if (argsc <= 1)
+    {
+        // no arguments
+    }
+    else
+    {
+        int i = 1;
+        while (i < argsc)
+        {
+            char *arg = argsv[i];
+            if (strcmp(arg, "-p") == 0)
+            {
+                i++;
+                if (i == argsc)
+                {
+                    // error empty arg
+                }
+                else
+                {
+                    port_Address = atoi(argsv[i]);
+                }
+            }
+            else if (strcmp(arg, "-ip") == 0)
+            {
+                i++;
+                if (i == argsc)
+                {
+                    // error empty arg
+                }
+                else
+                {
+                    ip_address = argsv[i];
+                }
+            }
+            else if (strcmp(arg, "-algo") == 0)
+            {
+                i++;
+                if (i == argsc)
+                {
+                    // error empty arg
+                }
+                else
+                {
+                    // check if value is reno or cubic
+                    tcp_algo = argsv[i];
+                }
+            }
+            i++;
+        }
+    }
+
+    //// finished args reading
+
+    // The variable to store the socket file descriptor.
+    int sock = -1;
+
+    // The variable to store the server's address.
+    struct sockaddr_in server;
+
+    // Create a message to send to the server.
+    // char *message = "Hello from client";
+
+    // // Create a buffer to store the received message.
+    // char buffer[1024] = {0};
+
+    // Reset the server structure to zeros.
+    memset(&server, 0, sizeof(server));
+
+    // Try to create a TCP socket (IPv4, stream-based, default protocol).
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    // If the socket creation failed, print an error message and return 1.
+    if (sock == -1)
+    {
+        perror("socket(2)");
+        return 1;
+    }
+
+    int opt = 1;
+
+    // Set the socket option to reuse the server's address.
+    // This is useful to avoid the "Address already in use" error message when restarting the server.
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        perror("setsockopt(2)");
+        close(sock);
+        return 1;
+    }
+
+    // Set the TCP congestion algorithm option for this socket
+    char alg_buf[256];
+    socklen_t alg_buf_len;
+
+    strcpy(alg_buf, tcp_algo);
+    alg_buf_len = sizeof(alg_buf);
+
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, alg_buf, alg_buf_len) != 0)
+    {
+        // Option set failed
+        perror("setsockopt");
+        close(sock);
+        return -1;
+    }
+
+    // Convert the server's address from text to binary form and store it in the server structure.
+    // This should not fail if the address is valid (e.g. "127.0.0.1").
+    if (inet_pton(AF_INET, ip_address, &server.sin_addr) <= 0)
+    {
+        perror("inet_pton(3)");
+        close(sock);
+        return 1;
+    }
+
+    // Set the server's address family to AF_INET (IPv4).
+    server.sin_family = AF_INET;
+
+    // Set the server's port to the defined port. Note that the port must be in network byte order,
+    // so we first convert it to network byte order using the htons function.
+    server.sin_port = htons(port_Address);
+
+    fprintf(stdout, "client: Connecting to %s:%d...\n", ip_address, port_Address);
+
+    // Try to connect to the server using the socket and the server structure.
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        perror("connect(2)");
+        close(sock);
+        return 1;
+    }
+    fprintf(stdout, "Successfully connected to the server!\n"
+                    "Sending message to the server: %s\n",
+            "message");
+
+    do
+    {
+        // Try to send the message to the server using the socket.
+        int bytes_sent = send(sock, rnd_file_buffer, strlen(rnd_file_buffer) + 1, 0);
+
+        // If the message sending failed, print an error message and return 1.
+        // If no data was sent, print an error message and return 1. Only occurs if the connection was closed.
+        if (bytes_sent <= 0)
+        {
+            perror("send(2): ");
+            close(sock);
+            return 1;
+        }
+
+        fprintf(stdout, "Client: Sent %d bytes to the server!\n", bytes_sent);
+    } while (0);
+
+    // message recive code
+    /*
+        // Try to receive a message from the server using the socket and store it in the buffer.
+    int bytes_received = recv(sock, buffer, sizeof(buffer), 0);
+
+    // If the message receiving failed, print an error message and return 1.
+    // If no data was received, print an error message and return 1. Only occurs if the connection was closed.
+    if (bytes_received <= 0)
+    {
+        perror("recv(2)");
+        close(sock);
+        return 1;
+    }
+
+
+    // Ensure that the buffer is null-terminated, no matter what message was received.
+    // This is important to avoid SEGFAULTs when printing the buffer.
+    if (buffer[1024 - 1] != '\0')
+        buffer[1024 - 1] = '\0';
+
+    // Print the received message.
+    fprintf(stdout, "Got %d bytes from the server, which says: %s\n", bytes_received, buffer);
+    */
+
+    // Close the socket with the server.
+    close(sock);
+
+    fprintf(stdout, "Connection closed!\n");
+
+    // Return 0 to indicate that the client ran successfully.
+    return 0;
+}
+
+/// @brief asks the user if he wants to continue, y is yes else no
+/// @return 1 if user anserwed yes else 0
+int user_cont()
+{
+    char ans = 0;
+    printf("do you want to send file again motherfucker?");
+    scanf("%c", &ans);
+    if (ans == 'y')
+        return 1;
+    else
+    {
+        return 0;
+    }
+}
+
+/* @brief
+A random data generator function based on srand() and rand().
+* @param size
+The size of the data to generate (up to 2^32 bytes).
+* @return
+A pointer to the buffer.
+*/
+char *util_generate_random_data(unsigned int size)
+{
+    char *buffer = NULL;
+    // Argument check.
+    if (size == 0)
+        return NULL;
+    buffer = (char *)calloc(size, sizeof(char));
+    // Error checking.
+    if (buffer == NULL)
+        return NULL;
+    // Randomize the seed of the random number generator.
+    srand(time(NULL));
+    for (unsigned int i = 0; i < size; i++)
+        *(buffer + i) = ((unsigned int)rand() % 256);
+    return buffer;
+}
